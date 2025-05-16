3c220e3225f79c866baaea3903b39683137a11dc
ElBrunoX.js <brunolok79@gmail.com>
2025-05-15 23:29:00 -0300

    se creo el metodo getAllBooks en la clase MongoBookRepository ( Clase que controla la comunicacion con la base de dato ) para obtener todos los libros en la base de dato

--- src/books/infrastructure/mongoBookRepository.ts
+++ src/books/infrastructure/mongoBookRepository.ts
@@ -3,8 +3,21 @@ import { BooksRepository } from "../domain/booksRepository";
 import { Books } from "../domain/books";
 
 export class MongoBookRepository implements BooksRepository {
+  /*
+  método de repositorio que es para crear o almacenar un nuevo libro
+  */
+
   async createBook(book: Books): Promise<void> {
     const newBook = new BookModel(book);
     await newBook.save();
+  }
+
+  /*
+  método para obtener todo los libros en la base de datos
+   */
+
+  async getAllBooks(): Promise<Books[]> {
+    const books = await BookModel.find();
+    return books;
   }
 }
