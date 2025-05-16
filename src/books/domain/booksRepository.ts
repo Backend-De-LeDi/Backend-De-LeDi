ac9b8f423f226abcebdae1e33582e2dc58785764
ElBrunoX.js <brunolok79@gmail.com>
2025-05-15 23:21:51 -0300

    se añadio un nuevo tipo de metodo **getAllBooks** que permite hacer que las clases utilizen solo lo especificado en su repositorio

--- src/books/domain/booksRepository.ts
+++ src/books/domain/booksRepository.ts
@@ -2,7 +2,7 @@ import { Books } from "./books";
 
 export interface BooksRepository {
   createBook(book: Books): Promise<void>;
-  // getAllBooks(): Promise<Books[]>;
+  getAllBooks(): Promise<Books[]>;
   // getBookById(id: ObjectId): Promise<Books | null>;
   // delete(id: ObjectId): Promise<void>;
 }
