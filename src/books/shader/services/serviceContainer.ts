1743386fe6cc935b0ecdffe5a4058abd433669ea
ElBrunoX.js <brunolok79@gmail.com>
2025-05-17 23:17:42 -0300

    se añadio dos atributos donde se juntan las clases deleteBookById y getBooksById con su respectivo repositorio que el que utilaza la base de dato de mongoAtlas

--- src/books/shader/services/serviceContainer.ts
+++ src/books/shader/services/serviceContainer.ts
@@ -1,5 +1,7 @@
 import { BookCreate } from "../../application/createBook/createBook";
+import { DeleteBook } from "../../application/deleteBook/deleteBook";
 import { getAllBooks } from "../../application/getAllBooks/getAllBooks";
+import { getBooksById } from "../../application/getBookById/getBookById";
 import { MongoBookRepository } from "../../infrastructure/mongoBookRepository";
 
 const booksRepository = new MongoBookRepository();
@@ -8,5 +10,7 @@ export const serviceContainer = {
   book: {
     createBook: new BookCreate(booksRepository),
     getAllBooks: new getAllBooks(booksRepository),
+    deleteBook: new DeleteBook(booksRepository),
+    getBooksById: new getBooksById(booksRepository),
   },
 };
