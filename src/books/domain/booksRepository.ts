06a6c0efc8c4f2788ab860a222d1b7f2f03b53d3
ElBrunoX.js <brunolok79@gmail.com>
2025-05-17 22:54:40 -0300

    se amplio la interfas del repositorio que contiene los metodos que utiliza el archivos mongoBookRepository.ts en si clase principal, los metodo implementados son deleteBook y getBookById

--- src/books/domain/booksRepository.ts
+++ src/books/domain/booksRepository.ts
@@ -1,8 +1,12 @@
+import { Types } from "mongoose";
 import { Books } from "./books";
+import { SearchedBook } from "../../types/bookTypes";
 
 export interface BooksRepository {
   createBook(book: Books): Promise<void>;
   getAllBooks(): Promise<Books[]>;
+  deleteBook(id: Types.ObjectId): Promise<void>;
+  getBookById(id: Types.ObjectId): Promise<SearchedBook | null>;
   // getBookById(id: ObjectId): Promise<Books | null>;
   // delete(id: ObjectId): Promise<void>;
 }
