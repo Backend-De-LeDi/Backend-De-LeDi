cdc9db1ef9ffc7357ab9075d47ae4db4896e8722
ElBrunoX.js <brunolok79@gmail.com>
2025-05-28 12:08:45 -0300

    se creo una clase para realizar busquedas inteligentes de los libros

---
+++ src/books/application/getIntelligenceBooks.ts
@@ -0,0 +1,10 @@
+import { BooksRepository } from "../domain/booksRepository";
+import { Books } from "../domain/books";
+
+export class GetIntelligenceBook {
+  constructor(private repository: BooksRepository) {}
+
+  async run(query: string): Promise<Books[]> {
+    return this.repository.getIntelligenceBook(query);
+  }
+}
