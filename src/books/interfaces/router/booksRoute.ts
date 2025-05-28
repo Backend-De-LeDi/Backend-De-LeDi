30fb852d5f11afabd49e1f3a29ef7ad9ed2b7f21
ElBrunoX.js <brunolok79@gmail.com>
2025-05-28 12:18:38 -0300

    se crearon las rutas para obtener libros pro query, contenido por id y categoria

--- src/books/interfaces/router/booksRoute.ts
+++ src/books/interfaces/router/booksRoute.ts
@@ -29,4 +29,15 @@ route.delete("/book/:id", (req: Request, res: Response) => {
   controller.deleteBook(req, res);
 });
 
+route.get("/books/:query", (req: Request, res: Response) => {
+  controller.getIntelligenceBooks(req, res);
+});
+
+route.get("/booksByCategory/:category", (req: Request, res: Response) => {
+  controller.getBookByCategory(req, res);
+});
+
+route.get("/book/content/:id", (req: Request, res: Response) => {
+  controller.getContentBookById(req, res);
+});
 export default route;
