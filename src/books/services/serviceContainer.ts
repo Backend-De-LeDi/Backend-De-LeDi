b007c4925f3a66bfebcd82230c0205345e591fdb
ElBrunoX.js <brunolok79@gmail.com>
2025-05-28 12:20:24 -0300

    se juntaron las aplicaciones y los metodos del repositorio en el object serviceContainer en su atributo book

--- src/books/services/serviceContainer.ts
+++ src/books/services/serviceContainer.ts
@@ -1,16 +1,22 @@
 import { BookCreate } from "../application/createBook";
 import { DeleteBook } from "../application/deleteBook";
-import { getAllBooks } from "../application/getAllBooks";
-import { getBooksById } from "../application/getBookById";
+import { GetAllBooks } from "../application/getAllBooks";
+import { GetBooksById } from "../application/getBookById";
+import { GetBookByCategory } from "../application/getBooksByCategory";
+import { GetIntelligenceBook } from "../application/getIntelligenceBooks";
 import { MongoBookRepository } from "../infrastructure/mongoBookRepository";
+import { getContentBookById } from "../application/getContentBookByCategory";
 
 const booksRepository = new MongoBookRepository();
 
 export const serviceContainer = {
   book: {
     createBook: new BookCreate(booksRepository),
-    getAllBooks: new getAllBooks(booksRepository),
+    getAllBooks: new GetAllBooks(booksRepository),
     deleteBook: new DeleteBook(booksRepository),
-    getBooksById: new getBooksById(booksRepository),
+    getBooksById: new GetBooksById(booksRepository),
+    getIntelligenceBook: new GetIntelligenceBook(booksRepository),
+    getBooksByCategory: new GetBookByCategory(booksRepository),
+    getContentById: new getContentBookById(booksRepository),
   },
 };
