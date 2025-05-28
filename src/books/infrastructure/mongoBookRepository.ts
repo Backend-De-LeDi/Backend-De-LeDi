e8aa9154d913a9d27b21c36a06944e89b148b7b9
ElBrunoX.js <brunolok79@gmail.com>
2025-05-28 12:15:13 -0300

    se crearon los metodos de comunicacion con la base de datos para obtener la informacion requeria de libros por categoria, por el buscador inteligente y si contenido por id

--- src/books/infrastructure/mongoBookRepository.ts
+++ src/books/infrastructure/mongoBookRepository.ts
@@ -2,7 +2,8 @@ import { BookModel } from "./model/books.model";
 import { BooksRepository } from "../domain/booksRepository";
 import { Books } from "../domain/books";
 import { Types } from "mongoose";
-import { SearchedBook } from "../../types/bookTypes";
+import { BookContent, SearchedBook } from "../../types/bookTypes";
+import ContentBook from "./model/contentBookModel";
 
 export class MongoBookRepository implements BooksRepository {
   //método de repositorio que es para crear o almacenar un nuevo libro
@@ -29,5 +30,37 @@ export class MongoBookRepository implements BooksRepository {
       return null;
     }
     return book;
+  }
+
+  async getIntelligenceBook(query: string): Promise<Books[]> {
+    // const books = await BookModel.find({ tile: { $search: `"${query}"` } });
+    let resBooks = await BookModel.find({ title: { $regex: query, $options: "i" } });
+
+    if (resBooks.length === 0) {
+      resBooks = await BookModel.find({ author: { $regex: query, $options: "i" } });
+    }
+
+    if (resBooks.length === 0) {
+      resBooks = await BookModel.find({
+        $or: [{ descriptions: { $regex: query, $options: "i" } }, { summary: { $regex: query, $options: "i" } }],
+      });
+    }
+
+    return resBooks;
+  }
+
+  async getBooksByCategory(category: string[]): Promise<Books[]> {
+    const books = await BookModel.find({ category: { $in: category } });
+    return books;
+  }
+
+  async getContentBookById(id: Types.ObjectId): Promise<string | null> {
+    const fullBook: SearchedBook | null = await BookModel.findById(id);
+    if (fullBook === null) return null;
+
+    const contentBook = await ContentBook.findById(fullBook.content);
+    if (contentBook === null) return null;
+
+    return contentBook.path;
   }
 }
