92f4238d93f68762941f430f70bb8a5305c0f1e3
ElBrunoX.js <brunolok79@gmail.com>
2025-06-04 02:24:34 -0300

    se crearon las validaciones minimas de para poder subir un libro

---
+++ src/shared/validations/book.validations.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const bookSchema = z.object({
+  title: z.string().min(1, { message: "El título es obligatorio" }),
+  descriptions: z.string().min(1, { message: "La descripción es obligatoria" }),
+  author: z.string().min(1, { message: "El autor es obligatorio" }),
+  category: z.array(z.string()).nonempty({ message: "Debe haber al menos una categoría" }),
+  available: z.boolean().refine((val) => typeof val === "boolean", {
+    message: "El campo 'available' debe ser un booleano (true o false)",
+  }),
+  language: z.string().min(1, { message: "El idioma es obligatorio" }),
+  summary: z.string().min(1, { message: "El resumen es obligatorio" }),
+});
