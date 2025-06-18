699b2ef2f118a21aa3a20f4a168e01da15b1f5b0
ElBrunoX.js <brunolok79@gmail.com>
2025-06-18 02:40:56 -0300

    se modifico el caso de uso de createBook cambiando los parametros que se les pasamos: auhtor id content: id y url  tipado correctamente

--- src/books/application/createBook.ts
+++ src/books/application/createBook.ts
@@ -2,23 +2,26 @@ import { Books } from "../domain/books";
 import { BooksRepository } from "../domain/booksRepository";
 import type { Types } from "mongoose";
 import { coverImage } from "../../types/bookTypes";
+import { ContentBook } from "../../types/contentBook";
 
+//clase que limita al Repositorio a realizar el almacenamiento del libro
 export class BookCreate {
   constructor(private repository: BooksRepository) {}
 
   async run(
     title: string,
     descriptions: string,
-    author: string,
-    userId: Types.ObjectId,
+    author: Types.ObjectId,
     category: string[],
     language: string,
     available: boolean,
-    content: Types.ObjectId,
+    content: ContentBook,
     coverImage: coverImage,
     summary: string
   ): Promise<void> {
-    const book = new Books(title, descriptions, author, userId, category, language, available, content, coverImage, summary);
+    const book = new Books(title, descriptions, author, category, language, available, content, coverImage, summary);
+
+    // console.log('datos ingresados al método run de la clase BookCreate:',book);
 
     await this.repository.createBook(book);
   }
