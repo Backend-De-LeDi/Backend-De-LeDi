e80e37702b87aac73d502d6a78b0d1eb0d020a30
ElBrunoX.js <brunolok79@gmail.com>
2025-06-18 02:45:16 -0300

    se modifico el modelo de la estructura para almacenarlo en la base de datos content con la id del libro que se utiliza para la manipulacion del documento y la url para obtener el contenido del libro author como id haciendo referencia a la coleccion autor

--- src/books/infrastructure/model/books.model.ts
+++ src/books/infrastructure/model/books.model.ts
@@ -1,22 +1,25 @@
-import { title } from "process";
 import { IBook } from "../../../types/bookTypes";
 import { Schema, model } from "mongoose";
 
+// definimos el modelo de estructura para almacenar los datos
 const bookSchema = new Schema<IBook>({
   title: { type: String, required: true },
-  userId: { type: Schema.Types.ObjectId, ref: "users" },
-  author: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "authors" },
   descriptions: { type: String, required: true },
   category: [{ type: String, required: true }],
   language: { type: String, required: true },
   available: { type: Boolean, default: true },
   createdAt: { type: Date, default: Date.now },
-  content: { type: Schema.Types.ObjectId, ref: "ContentBooks", required: true },
-  pathInternal: { type: String, require: true },
+  content: {
+    type: {
+      idContentBook: { type: String, required: true },
+      url_secura: { type: String, required: true },
+    },
+  },
   summary: { type: String, require: true },
   coverImage: {
     type: {
-      id_image: { type: String, required: true },
+      idCoverImage: { type: String, required: true },
       url_secura: { type: String, required: true },
     },
   },
