d11dec0e186e9e6b1b395a5f41f2f3d36a109c49
ElBrunoX.js <brunolok79@gmail.com>
2025-06-18 02:42:40 -0300

    se cambio el dominio: autor es una id de mongo ahora para la referencia content se simplifico ahora contiene solo la id y la url para aceder y manipularlo en cloudinary

--- src/books/domain/books.ts
+++ src/books/domain/books.ts
@@ -1,15 +1,17 @@
-import type { Types } from "mongoose";
 import { coverImage } from "../../types/bookTypes";
+import { ContentBook } from "../../types/contentBook";
+import type { Types } from "mongoose";
+
+// definimos el dominio ( como se va almacenar los libros )
 export class Books {
   constructor(
     public title: string,
     public descriptions: string,
-    public author: string,
-    public userId: Types.ObjectId,
+    public author: Types.ObjectId,
     public category: string[],
     public language: string,
     public available: boolean,
-    public content: Types.ObjectId,
+    public content: ContentBook,
     public coverImage: coverImage,
     public summary: string
   ) {}
