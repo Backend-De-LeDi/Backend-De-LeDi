5b744a57a2da02269b8749af9eca8a95c1038d48
PC del instituto <brunolok79@gmail.com>
2025-06-26 12:34:46 -0300

    se realizo caambios minimos

--- src/config/configEnv.ts
+++ src/config/configEnv.ts
@@ -1,22 +1,20 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const ENV: Env = {
-  PORT: Number(process.env.PORT) || 3000,
-  MONGO_URL: String(process.env.URL_DB),
-  CLOUD_NAME: process.env.CLOUD_NAME,
-  API_KEY: process.env.API_KEY,
-  API_SECRET: process.env.API_SECRET,
-  ROUTE_EXTENSION: process.env.ABSOLUTE_PATH,
-};
-
 interface Env {
   PORT: number;
   MONGO_URL: string;
   CLOUD_NAME: string | undefined;
   API_KEY: string | undefined;
   API_SECRET: string | undefined;
-  ROUTE_EXTENSION: string | undefined;
 }
+
+const ENV: Env = {
+  PORT: Number(process.env.PORT) || 3000,
+  MONGO_URL: String(process.env.URL_DB),
+  CLOUD_NAME: process.env.CLOUD_NAME,
+  API_KEY: process.env.API_KEY,
+  API_SECRET: process.env.API_SECRET,
+};
 
 export default ENV;
