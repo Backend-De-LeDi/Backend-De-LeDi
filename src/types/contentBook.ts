e42a0bff2416de88558a8e4b39fc67bf1cc6bca5
ElBrunoX.js <brunolok79@gmail.com>
2025-06-29 12:24:45 -0300

    se añadió la interfaz ContentAudioBook, omitiendo idContentBook y añadiendo idAudio

--- src/types/contentBook.ts
+++ src/types/contentBook.ts
@@ -7,3 +7,7 @@ export interface FullContentBook extends ContentBook {
   _id: string;
   __v: number;
 }
+
+export interface ContentAudioBook extends Omit<ContentBook, "idContentBook"> {
+  idAudio: string;
+}
