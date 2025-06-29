f9d4d2aa91acd86c281f07e6fc8bf419b2b23fbe
ElBrunoX.js <brunolok79@gmail.com>
2025-06-29 12:15:25 -0300

    se eliminó la clase getContentBookById para simplificar la gestión de libros por categoría

--- src/books/application/getContentBookByCategory.ts
+++
@@ -1,9 +0,0 @@
-import { Types } from "mongoose";
-import { BooksRepository } from "../domain/booksRepository";
-
-export class getContentBookById {
-  constructor(private repository: BooksRepository) {}
-  async run(id: Types.ObjectId): Promise<string | null> {
-    return await this.repository.getContentBookById(id);
-  }
-}
