c0788da7e9cb462fcd9eb9cd64b0d3990e268c3d
ElBrunoX.js <brunolok79@gmail.com>
2025-06-29 12:18:29 -0300

    se actualizó el esquema del modelo de libro para reemplazar 'category' por 'subgenre' y se corrigieron errores en los campos 'createdAt' y 'genreType'

--- src/books/infrastructure/model/books.model.ts
+++ src/books/infrastructure/model/books.model.ts
@@ -2,29 +2,32 @@ import { IBook } from "../../../types/bookTypes";
 import { Schema, model } from "mongoose";
 
 // definimos el modelo de estructura para almacenar los datos
-const bookSchema = new Schema<IBook>({
-  title: { type: String, required: true },
-  author: { type: Schema.Types.ObjectId, ref: "authors" },
-  descriptions: { type: String, required: true },
-  category: [{ type: String, required: true }],
-  language: { type: String, required: true },
-  available: { type: Boolean, default: true },
-  createdAt: { type: Date, default: Date.now },
-  content: {
-    type: {
-      idContentBook: { type: String, required: true },
-      url_secura: { type: String, required: true },
+const bookSchema = new Schema<IBook>(
+  {
+    title: { type: String, required: true },
+    author: [{ type: Schema.Types.ObjectId, ref: "authors" }],
+    descriptions: { type: String, required: true },
+    subgenre: [{ type: String, required: true }],
+    language: { type: String, required: true },
+    available: { type: Boolean, default: true },
+    yearBook: { type: Date, default: Date.now },
+    content: {
+      type: {
+        idContentBook: { type: String, required: true },
+        url_secura: { type: String, required: true },
+      },
     },
-  },
-  summary: { type: String, require: true },
-  coverImage: {
-    type: {
-      idCoverImage: { type: String, required: true },
-      url_secura: { type: String, required: true },
+    coverImage: {
+      type: {
+        idCoverImage: { type: String, required: true },
+        url_secura: { type: String, required: true },
+      },
     },
+    genreType: { type: String, require: true },
   },
-});
+  { timestamps: true }
+);
 
-const BookModel = model<IBook>("Book", bookSchema);
+const BookModel = model<IBook>("Books", bookSchema);
 
 export { BookModel };
