3cacf7375e001d4e517f8a6b6254caebc661f446
ElBrunoX.js <brunolok79@gmail.com>
2025-06-29 12:14:14 -0300

    se agregó la palabra clave await en el método run de la clase DeleteBook para asegurar la correcta ejecución de la eliminación del libro

--- src/books/application/deleteBook.ts
+++ src/books/application/deleteBook.ts
@@ -4,6 +4,6 @@ import { Types } from "mongoose";
 export class DeleteBook {
   constructor(private repository: BooksRepository) {}
   async run(id: Types.ObjectId): Promise<void> {
-    this.repository.deleteBook(id);
+    await this.repository.deleteBook(id);
   }
 }
