1db6e8f360224c388b1151f29581e8eb6a0ed2b0
ElBrunoX.js <brunolok79@gmail.com>
2025-06-29 12:22:08 -0300

    se actualizó la utilidad parseFormData para manejar correctamente el campo 'subgenre' y convertir los IDs de 'author' a ObjectId

--- src/shared/utils/parseFormData.ts
+++ src/shared/utils/parseFormData.ts
@@ -1,4 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
+import { Types } from "mongoose";
 
 export const parseFormData = (req: Request, res: Response, next: NextFunction): void => {
   req.body = req.body || {}; // 🔹 Asegura que req.body no sea undefined
@@ -7,10 +8,14 @@ export const parseFormData = (req: Request, res: Response, next: NextFunction): 
     try {
       if (key === "available") {
         req.body[key] = req.body[key] === "true"; // Convertir a booleano
-      } else if (key === "category") {
+      } else if (key === "subgenre") {
         req.body[key] = Array.isArray(req.body[key])
           ? req.body[key] // Si ya es un array, mantenerlo igual
           : [req.body[key]]; // Si es un string, convertirlo en array
+      } else if (key === "author") {
+        const val = req.body[key];
+        const ids = Array.isArray(val) ? val : [val];
+        req.body[key] = ids.map((id) => Types.ObjectId.createFromHexString(id));
       }
     } catch {
       // Si hay un error, mantener el valor original
