a4c7a689175bcacd0c8909caffc8a6c928b0bc01
ElBrunoX.js <brunolok79@gmail.com>
2025-06-29 12:14:51 -0300

    se eliminó la clase GetBookByCategory y su implementación para simplificar la gestión de libros por categoría

--- src/books/application/getBooksByCategory.ts
+++
@@ -1,8 +0,0 @@
-import { BooksRepository } from "../domain/booksRepository";
-export class GetBookByCategory {
-  constructor(private repository: BooksRepository) {}
-
-  async run(category: string[]) {
-    return await this.repository.getBooksByCategory(category);
-  }
-}
