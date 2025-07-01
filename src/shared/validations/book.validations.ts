f5b8805a8ad03c337bbeb9f4a077e8dbcd35f303
PC del instituto <brunolok79@gmail.com>
2025-07-01 13:42:44 -0300

    se mejoró la legibilidad del esquema de validaciones de libros al reorganizar comentarios y el código

--- src/shared/validations/book.validations.ts
+++ src/shared/validations/book.validations.ts
@@ -1,17 +1,78 @@
 import { z } from "zod";
 import { Types } from "mongoose";
 
-const isObjectId = (val: unknown): val is Types.ObjectId => val instanceof Types.ObjectId;
+const isObjectId = (val: unknown): val is Types.ObjectId =>
+  val instanceof Types.ObjectId;
 
 export const bookSchema = z.object({
-  title: z.string().min(1, { message: "El título es obligatorio" }),
-  descriptions: z.string().min(1, { message: "La descripción es obligatoria" }),
-  subgenre: z.array(z.string()).nonempty({ message: "Debe haber al menos un subgénero" }),
+  
+  // * validaciones para el titulo
+
+  title: z
+    .string({ message: "el titulo no puede ser un numero " })
+    .min(1, { message: "El título es obligatorio" })
+    .max(100, { message: "un titulo solo puede tener 100 caracteres" }),
+
+  // * --------------------------------------------------------------------------------------------
+
+  // * validaciones para la descripción
+
+  descriptions: z
+    .string({ message: "la descripción no puede ser un numero" })
+    .min(1, { message: "La descripción es obligatoria" })
+    .max(4000, {
+      message:
+        "el limite de caracteres que puede tener una descripción es de 4000 caracteres",
+    }),
+
+  // * --------------------------------------------------------------------------------------------
+
+  // * validaciones del campo subgénero
+
+  subgenre: z
+    .array(
+      z
+        .string({ message: "los subgénero no pueden ser de valores numérico" })
+        .max(50, {
+          message: "un subgénero solo puede tener 50 caracteres como máximo",
+        })
+    )
+    .nonempty({ message: "Debe haber al menos un subgénero" }),
+
+  // * --------------------------------------------------------------------------------------------
+
+  // * validación de estado del libro si es valido o no en la plataforma
+
   available: z.boolean().refine((val) => typeof val === "boolean", {
-    message: "El campo 'available' debe ser un booleano (true o false)",
+    message: "El campo debe ser un valor de verdad si es valido o no",
   }),
-  language: z.string().min(1, { message: "El idioma es obligatorio" }),
-  genreType: z.string().min(1, { message: "el genero principal es obligatorio " }),
+  language: z
+    .string()
+    .min(1, { message: "El idioma es obligatorio" })
+    .max(3, {
+      message: `
+    debe ingresar las siglas del idioma, 
+    Ejemplo:
+    'es': español,
+    'en': ingles`,
+    }),
+
+  // * --------------------------------------------------------------------------------------------
+
+  // * validación de tipo de genero principal
+
+  genreType: z
+    .string()
+    .min(1, { message: "el genero principal es obligatorio " })
+    .max(50, {
+      message:
+        "el máximo de caracteres que puede tener el genero principal es de 50",
+    }),
+
+  // * --------------------------------------------------------------------------------------------
+
+  // * validación de la id del author, referencias entre autor con libro
+
   author: z
     .array(
       z.union([
@@ -22,6 +83,10 @@ export const bookSchema = z.object({
       ])
     )
     .nonempty({
-      message: "Este campo requiere uno o más autores o autoras. No puede quedar vacío.",
-    }),
+      message:
+        "Este campo requiere uno o más autores o autoras. No puede quedar vacío.",
+    })
+    .min(24, { message: "debes ingresar una id valida" }),
+
+  // * --------------------------------------------------------------------------------------------
 });
