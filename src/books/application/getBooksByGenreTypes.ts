1d61010be4da523913c340a58b8b1d13c92807d9
ElBrunoX.js <brunolok79@gmail.com>
2025-07-01 22:41:04 -0300

    se añadió el método run a la clase GetBooksByGenreTypes para obtener libros por tipo de género

--- src/books/application/getBooksByGenreTypes.ts
+++ src/books/application/getBooksByGenreTypes.ts
@@ -1,8 +1,17 @@
+// * importación del domino que es lo que se devolverá en la aplicación de uso GetBookByGenreTypes
 import { Books } from "../domain/books";
+
+// * importación del repositorio para hacer el uso de su método getBookGenreTypes 
 import { BooksRepository } from "../domain/booksRepository";
 
-class GetBooksByGenreTypes {
+// * aplicación de uso que recibe un repositorio para hacer uso de su método
+export class GetBooksByGenreTypes {
+
   constructor(private repository: BooksRepository) {}
 
-  async run(): Promise<Books[]> {}
+  async run(genreType: string): Promise<Books[]> {
+  
+    return await this.repository.getBookByGenreTypes(genreType);
+  
+  }
 }
