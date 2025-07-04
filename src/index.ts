5e70bc87ee28a6ec8fd178d2f6a6250e9da961a5
ElBrunoX.js <brunolok79@gmail.com>
2025-07-04 20:57:07 -0300

    cambios mínimos

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
+// ? importación de módulos necesarios
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import fs from "fs";
 import path from "path";
 import chalk from "chalk";
-import morgan from "morgan";
-import connections from "./shared/config/db/database";
-import * as dotenv from "dotenv";
-dotenv.config();
-import ENV from "./shared/config/configEnv";
 
+// ? importación de módulos locales
+import ENV from "./shared/config/configEnv";
+import connections from "./shared/config/db/database";
 import { userRoutes } from "./userService/interfaces/routes/userService.routes";
 import bookRouter from "./books/interfaces/router/booksRoute";
 import { authRoutes } from "./authService/interfaces/routes/auth.routes";
@@ -16,21 +16,23 @@ import session from "express-session";
 import audiobookRouter from "./audiobooks/interface/routers/audiobooksRouter";
 import { progressRouter } from "./userPogressBooks/interface/routes/bookProgress.routes";
 
+// ? creación de la aplicación Express
 const app = express();
-connections();
 
+// ? configuración del directorio de subida de archivos
 const fileUpload = path.join(__dirname, "./uploads");
 
-if (!fs.existsSync(fileUpload)) {
-  fs.mkdirSync(fileUpload, { recursive: true });
-}
+// ? verificación de la existencia del directorio de subida de archivos
+if (!fs.existsSync(fileUpload)) fs.mkdirSync(fileUpload, { recursive: true });
 
+// ? configuración de middlewares
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./src/uploads"));
 
+// ? configuración de sesiones
 app.use(
   session({
     secret: "tu_clave_secreta",
@@ -43,12 +45,17 @@ app.use(
   })
 );
 
+// ? configuracion de rutas
 app.use(userRoutes);
 app.use(authRoutes);
 app.use(progressRouter);
 app.use(bookRouter);
 app.use(audiobookRouter);
 
-app.listen(Number(ENV.PORT), () => {
-  console.log(` Servidor corriendo en el puerto ${ENV.PORT}`);
+// ? configuración de puerto
+app.listen(Number(ENV.PORT), async () => {
+  console.log();
+  console.log(chalk.green(`server is Running on http://localhost:${ENV.PORT}`));
+  console.log();
+  await connections();
 });
