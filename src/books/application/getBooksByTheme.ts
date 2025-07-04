f821b6d11d2bee7669528f157fbce2a90122b38b
PC del instituto <brunolok79@gmail.com>
2025-07-04 10:56:38 -0300

    se agregó la clase GetBookByTheme para implementar la lógica de obtención de libros por tema

---
+++ src/books/application/getBooksByTheme.ts
@@ -0,0 +1,23 @@
+import { Books } from "../domain/books";
+import { BooksRepository } from "../domain/booksRepository";
+
+// ? clase que guía al Repositorio a realizar la obtención de libros por tema
+export class GetBookByTheme {
+
+  // * constructor que recibe el repositorio de libros
+  constructor(private repository: BooksRepository) {}
+
+  // * método que se encarga de ejecutar la lógica de obtención de libros por tema
+  async run(theme: string[]): Promise<Books[]> {
+
+    // * se hace uso del repositorio para obtener los libros por tema
+  const books = await this.repository.getBookByTheme(theme);
+
+    // * si no hay libros, se retorna un array vacío
+  if (!books) return [];
+  
+
+    // * si hay libros, se retornan
+  return books;
+  }
+}
