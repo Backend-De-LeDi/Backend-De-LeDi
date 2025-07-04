7c64167e1766a297c6c69f8932ae5731cbe20e9c
ElBrunoX.js <brunolok79@gmail.com>
2025-07-04 20:07:29 -0300

    ubicación nueva del directorio de types

---
+++ src/types/bookTypes.ts
@@ -0,0 +1,33 @@
+import { Types, Document } from "mongoose";
+import { ContentBook, FullContentBook } from "./contentBook";
+
+export interface CoverImage {
+  idCoverImage: string;
+  url_secura: string;
+}
+
+export interface PropBooks extends Document {
+  title: string;
+  author: Types.ObjectId[];
+  summary: string;
+  subgenre: string[];
+  language: string;
+  available: boolean;
+  yearBook: Date;
+  synopsis: string;
+  theme: string[];
+}
+
+export interface IBook extends PropBooks {
+  content: ContentBook;
+  coverImage: CoverImage;
+}
+
+export interface SearchedBook extends IBook {
+  _id: string;
+  __v: number;
+}
+
+export interface BookContent extends Omit<SearchedBook, "content"> {
+  content: FullContentBook;
+}
