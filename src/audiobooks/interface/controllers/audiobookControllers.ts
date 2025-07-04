740c86af76c1cfbdf066cf14132f575da81ebb39
PC del instituto <brunolok79@gmail.com>
2025-07-04 10:55:57 -0300

    se cambió la propiedad genreType a theme en el método createAudioBook para mejorar la claridad del modelo

--- src/audiobooks/interface/controllers/audiobookControllers.ts
+++ src/audiobooks/interface/controllers/audiobookControllers.ts
@@ -11,19 +11,34 @@ import { deleteCoverImageInCloudinary } from "../../../shared/utils/deleteCoverI
 
 // * clase que contiene todo los controladores de Audiobook que se utilizaran en las rutas
 export class AudiobookControllers {
-
   // * método para crea un audio libro
   async createAudioBook(req: Request, res: Response): Promise<Response> {
     try {
-      const { title, author, descriptions, subgenre, available, language, yearBook, summary, genreType }: PropAudiobooks = req.body;
+      const {
+        title,
+        author,
+        descriptions,
+        subgenre,
+        available,
+        language,
+        yearBook,
+        summary,
+        theme,
+      }: PropAudiobooks = req.body;
 
       const files = req.files as { [key: string]: Express.Multer.File[] };
 
       const img = files.img[0];
       const audio = files.audio[0];
 
-      if (!audio) return res.status(400).json({ msg: "Faltan archivos con el contenido del libro" });
-      if (!img) return res.status(400).json({ msg: "Faltan archivos de la portada del libro " });
+      if (!audio)
+        return res
+          .status(400)
+          .json({ msg: "Faltan archivos con el contenido del libro" });
+      if (!img)
+        return res
+          .status(400)
+          .json({ msg: "Faltan archivos de la portada del libro " });
 
       const coverImageSaved = await uploadAudiobookCoverImagen(img.path);
       const audiobookSaved = await uploadAudiobook(audio.path);
@@ -33,11 +48,19 @@ export class AudiobookControllers {
 
         await fileDelete(audio.path);
 
-        return res.status(400).json({ msg: "no se pudo almacenar el contenido o la portada del libro" });
+        return res.status(400).json({
+          msg: "no se pudo almacenar el contenido o la portada del libro",
+        });
       }
 
-      const content = { idAudio: audiobookSaved.public_id, url_secura: audiobookSaved.secure_url };
-      const coverImage = { url_secura: coverImageSaved.secure_url, idCoverImage: coverImageSaved.public_id };
+      const content = {
+        idAudio: audiobookSaved.public_id,
+        url_secura: audiobookSaved.secure_url,
+      };
+      const coverImage = {
+        url_secura: coverImageSaved.secure_url,
+        idCoverImage: coverImageSaved.public_id,
+      };
 
       await serviceContainer.audiobooks.createAudioBooks.run(
         title,
@@ -49,7 +72,7 @@ export class AudiobookControllers {
         content,
         coverImage,
         summary,
-        genreType,
+        theme,
         yearBook
       );
 
@@ -65,7 +88,9 @@ export class AudiobookControllers {
       console.log();
       console.log(chalk.yellow(separator()));
 
-      return res.status(500).json({ msg: "Error inesperado por favor intente de nuevo mas tarde" });
+      return res
+        .status(500)
+        .json({ msg: "Error inesperado por favor intente de nuevo mas tarde" });
     }
   }
 
@@ -82,66 +107,86 @@ export class AudiobookControllers {
       console.log(error);
       console.log();
       console.log(chalk.yellow(separator()));
-      return res.json({ msg: "Erro inesperado por favor intente de nuevo mas tarde" }).status(500);
+      return res
+        .json({ msg: "Erro inesperado por favor intente de nuevo mas tarde" })
+        .status(500);
     }
   }
 
   // * método que permite el la eliminación de los audio libros en la base de datos
   async deleteAudiobook(req: Request, res: Response): Promise<Response> {
-    try{
-
+    try {
       const id = req.params.id;
-      
-      if (!Types.ObjectId.isValid(id)) return res.status(404).json({ msg: "para eliminar un audiolibro debes ingresar una id valida" });
-      
-      const idFormatted = Types.ObjectId.createFromHexString(id);
-       
-      const audiobookToDelete = await serviceContainer.audiobooks.getAudiobookById.run(idFormatted);
-      
-      if (!audiobookToDelete) return res.status(404).json({ msg: "no se encontró el libro para eliminar " });
-      
-      const deletingStatus = await serviceContainer.audiobooks.deleteAudiobook.run(idFormatted);
-      
-      if (!deletingStatus) return res.status(500).json({ msg: "no sea podido eliminar el Audiolibro de la base de datos" });
-      
-      return res.status(200).json({msg:'Audiolibro eliminado correctamente'})
 
-    }catch(error){
+      if (!Types.ObjectId.isValid(id))
+        return res.status(404).json({
+          msg: "para eliminar un audiolibro debes ingresar una id valida",
+        });
+
+      const idFormatted = Types.ObjectId.createFromHexString(id);
+
+      const audiobookToDelete =
+        await serviceContainer.audiobooks.getAudiobookById.run(idFormatted);
+
+      if (!audiobookToDelete)
+        return res
+          .status(404)
+          .json({ msg: "no se encontró el libro para eliminar " });
+
+      const deletingStatus =
+        await serviceContainer.audiobooks.deleteAudiobook.run(idFormatted);
+
+      if (!deletingStatus)
+        return res.status(500).json({
+          msg: "no sea podido eliminar el Audiolibro de la base de datos",
+        });
+
+      return res
+        .status(200)
+        .json({ msg: "Audiolibro eliminado correctamente" });
+    } catch (error) {
       console.log(chalk.yellow("Error en el controlador: deleteAudiobook"));
       console.log(chalk.yellow(separator()));
       console.log();
       console.log(error);
       console.log();
       console.log(chalk.yellow(separator()));
-      return res.json({ msg: "Erro inesperado por favor intente de nuevo mas tarde" }).status(500);
+      return res
+        .json({ msg: "Erro inesperado por favor intente de nuevo mas tarde" })
+        .status(500);
     }
-      
-    }
+  }
 
   // * método que permite obtener Audiolibro de la base de datos
-  async getAudioBookById(req:Request,res:Response): Promise<Response>{
-    try{
-
+  async getAudioBookById(req: Request, res: Response): Promise<Response> {
+    try {
       const id = req.params.id;
 
-      if (!Types.ObjectId.isValid(id)) return res.status(404).json({ msg: "para obtener un audiolibro debes ingresar una id valida" });
+      if (!Types.ObjectId.isValid(id))
+        return res.status(404).json({
+          msg: "para obtener un audiolibro debes ingresar una id valida",
+        });
 
       const idFormatted = Types.ObjectId.createFromHexString(id);
 
-      const audiobook = await serviceContainer.audiobooks.getAudiobookById.run(idFormatted);
+      const audiobook = await serviceContainer.audiobooks.getAudiobookById.run(
+        idFormatted
+      );
 
-      if(!audiobook)return res.status(404).json({msg:'libro no encontrado'})
+      if (!audiobook)
+        return res.status(404).json({ msg: "libro no encontrado" });
 
-      return res.status(200).json(audiobook)
-
-    }catch(error){
-    console.log(chalk.yellow("Error en el controlador: getAudioBookById"));
-    console.log(chalk.yellow(separator()));
-    console.log();
-    console.log(error);
-    console.log();
-    console.log(chalk.yellow(separator()));
-    return res.json({ msg: "Erro inesperado por favor intente de nuevo mas tarde" }).status(500);
-  }
+      return res.status(200).json(audiobook);
+    } catch (error) {
+      console.log(chalk.yellow("Error en el controlador: getAudioBookById"));
+      console.log(chalk.yellow(separator()));
+      console.log();
+      console.log(error);
+      console.log();
+      console.log(chalk.yellow(separator()));
+      return res
+        .json({ msg: "Erro inesperado por favor intente de nuevo mas tarde" })
+        .status(500);
+    }
   }
 }
