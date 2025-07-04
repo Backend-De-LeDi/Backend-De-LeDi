31c6aa7d8b0cd079d2a1b773a5891a791b6335c4
PC del instituto <brunolok79@gmail.com>
2025-07-04 10:55:37 -0300

    se cambió la propiedad genreType a theme en el esquema de AudioBook para mejorar la claridad del modelo

--- src/audiobooks/infrastructure/model/audiobookModel.ts
+++ src/audiobooks/infrastructure/model/audiobookModel.ts
@@ -11,11 +11,21 @@ const audioBookSchema = new Schema<IAudioBook>(
     subgenre: [{ type: String, require: true }],
     language: { type: String, required: true },
     available: { type: Boolean, default: true },
-    content: { type: { idAudio: { type: String, required: true }, url_secura: { type: String, required: true } } },
-    coverImage: { type: { idCoverImage: { type: String, required: true }, url_secura: { type: String, required: true } } },
+    content: {
+      type: {
+        idAudio: { type: String, required: true },
+        url_secura: { type: String, required: true },
+      },
+    },
+    coverImage: {
+      type: {
+        idCoverImage: { type: String, required: true },
+        url_secura: { type: String, required: true },
+      },
+    },
     summary: { type: String, required: true },
     yearBook: { type: Date, default: Date.now },
-    genreType: { type: String, required: true },
+    theme: [{ type: String, required: true }],
   },
   { timestamps: true } // * campo que almacena el tiempo de carga en la base de datos y cuando se actualiza en la bases de datos
 );
