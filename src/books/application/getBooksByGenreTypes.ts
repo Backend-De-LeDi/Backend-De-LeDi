450e379903686c31d8346a83c7d328c88df71d2f
PC del instituto <brunolok79@gmail.com>
2025-07-04 10:56:21 -0300

    se eliminó la clase GetBooksByGenreTypes y su archivo asociado para simplificar la estructura del código

--- src/books/application/getBooksByGenreTypes.ts
+++
@@ -1,8 +0,0 @@
-import { Books } from "../domain/books";
-import { BooksRepository } from "../domain/booksRepository";
-
-class GetBooksByGenreTypes {
-  constructor(private repository: BooksRepository) {}
-
-  async run(): Promise<Books[]> {}
-}
