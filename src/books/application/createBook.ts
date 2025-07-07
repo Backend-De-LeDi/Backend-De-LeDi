7ccbd6a4edcdabe099afba6024d778713eacbc23
ElBrunoX.js <brunolok79@gmail.com>
2025-07-07 20:35:27 -0300

    se simplifico la aplicación de uso para mejora lectura y uso

--- src/books/application/createBook.ts
+++ src/books/application/createBook.ts
@@ -1,30 +1,17 @@
 import { Books } from "../domain/books";
 import { BooksRepository } from "../domain/booksRepository";
 import type { Types } from "mongoose";
-import { CoverImage } from "../../types/bookTypes";
-import { ContentBook } from "../../types/contentBook";
 
 // ? clase que guía al Repositorio a realizar el almacenamiento del libro
-export class BookCreate {
+export class CreateBook {
+  // * inyectamos el repositorio de libros
   constructor(private repository: BooksRepository) {}
 
-  async run(
-    title: string,
-    summary: string,
-    author: Types.ObjectId[],
-    subgenre: string[],
-    language: string,
-    available: boolean,
-    content: ContentBook,
-    coverImage: CoverImage,
-    theme: string[],
-    synopsis: string,
-    genre: string,
-    level: string,
-    yearBook?: Date
-  ): Promise<void> {
-    const book = new Books(title, summary, author, subgenre, language, available, content, coverImage, synopsis, yearBook ?? new Date(), theme, genre, level);
+  // * método que crea un libro y lo almacena en la base de datos
+  async run(newBookData: Books): Promise<void> {
+    // * creamos una instancia del libro con los datos proporcionados
 
-    await this.repository.createBook(book);
+    // * llamamos al repositorio para crear el libro en la base de datos
+    await this.repository.createBook(newBookData);
   }
 }
