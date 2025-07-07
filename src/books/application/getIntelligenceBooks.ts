9969835545aac4ba3cbae7604f5dbfc3f8c91166
ElBrunoX.js <brunolok79@gmail.com>
2025-07-07 20:40:57 -0300

    se cambio la el tipo e dato que va a devolver el GetIntelligenceBook envés de Book es SearchedBook mas completo

--- src/books/application/getIntelligenceBooks.ts
+++ src/books/application/getIntelligenceBooks.ts
@@ -1,18 +1,15 @@
 import { BooksRepository } from "../domain/booksRepository";
 import { Books } from "../domain/books";
+import { SearchedBook } from "../../types/bookTypes";
 
 // ? Clase que guía al Repositorio a realizar la obtención de libros por inteligencia
 export class GetIntelligenceBook {
-
   // * Constructor que recibe el repositorio de libros
   constructor(private repository: BooksRepository) {}
 
   // * Método que ejecuta la obtención de libros por inteligencia
-  async run(query: string): Promise<Books[]> {
-
+  async run(query: string): Promise<SearchedBook[]> {
     // * Llama al repositorio para obtener los libros por inteligencia
     return await this.repository.getIntelligenceBook(query);
-
-    
   }
 }
