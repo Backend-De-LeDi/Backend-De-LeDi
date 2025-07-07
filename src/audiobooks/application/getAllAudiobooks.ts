219f23a8de4f986567c8836346bd5288203e11d8
ElBrunoX.js <brunolok79@gmail.com>
2025-07-07 20:15:49 -0300

    se creo la aplicación de uso para obtener todo los Audiolibros

---
+++ src/audiobooks/application/getAllAudiobooks.ts
@@ -0,0 +1,13 @@
+import { SearchedAudiobook } from "../../types/audiobookTypes";
+import { AudiobooksRepository } from "../domain/AudiobooksRepository";
+// ? Clase que guía al Repositorio a realizar la obtención de todos los libros
+export class GetAllAudiobooks {
+  // * Constructor que recibe el repositorio de libros
+  constructor(private repository: AudiobooksRepository) {}
+
+  // * Método que ejecuta la obtención de todos los libros
+  async run(): Promise<SearchedAudiobook[]> {
+    // * Llama al repositorio para obtener todos los libros
+    return await this.repository.getAllAudiobooks();
+  }
+}
