325e30a24c4a39f297e4ff6648ed1d3dd0a00eb5
ElBrunoX.js <brunolok79@gmail.com>
2025-07-07 20:21:39 -0300

    se aplico y se creo la similaridad de los Audiolibros par que sea parecidos al de los Libros

--- src/audiobooks/domain/audiobooks.ts
+++ src/audiobooks/domain/audiobooks.ts
@@ -1,4 +1,22 @@
-import { Books } from "../../books/domain/books";
+import { AudiobookCoverImage } from "../../types/audiobookTypes";
+import { ContentAudiobook } from "../../types/contentAudiobookTypes";
+import type { Types } from "mongoose";
 
-// definimos el dominio ( como se va almacenar los audiolibros )
-export class AudioBooks extends Books {}
+// ? Clase que representa un libro en el dominio de la aplicación
+export class Audiobook {
+  constructor(
+    public title: string,
+    public summary: string,
+    public author: Types.ObjectId[],
+    public subgenre: string[],
+    public language: string,
+    public available: boolean,
+    public contentAudiobook: ContentAudiobook,
+    public audiobookCoverImage: AudiobookCoverImage,
+    public theme: string[],
+    public genre: string,
+    public synopsis: string,
+    public level: string,
+    public yearBook: Date
+  ) {}
+}
