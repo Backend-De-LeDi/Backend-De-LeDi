c8fecc810158d8d67688ca5c3e402751730eb74a
ElBrunoX.js <brunolok79@gmail.com>
2025-07-07 20:46:59 -0300

    se ordeno el contenedor de servido que contiene la combinación de el repositorio de mongo con las aplicaciones de uso

--- src/shared/services/serviceContainer.ts
+++ src/shared/services/serviceContainer.ts
@@ -1,28 +1,28 @@
-import { BookCreate } from "../../books/application/createBook";
+import { CreateBook } from "../../books/application/createBook";
 import { DeleteBook } from "../../books/application/deleteBook";
 import { GetAllBooks } from "../../books/application/getAllBooks";
 import { GetBooksById } from "../../books/application/getBookById";
 import { GetBooksBySubgenre } from "../../books/application/getBooksBySubgenre";
 import { GetIntelligenceBook } from "../../books/application/getIntelligenceBooks";
-import { MongoBookRepository } from "../../books/infrastructure/mongoBookRepository";
+import { MongoAudiobookRepository } from "../../audiobooks/infrastructure/mongoAudiobookRepository";
 import { GetContentBookById } from "../../books/application/getContentBookById";
-import { CreateAudioBooks } from "../../audiobooks/applications/createAudiobooks";
-import { MongoAudiobooksRepository } from "../../audiobooks/infrastructure/mongoAudiobooksRepository";
-import { GetAllAudiobooks } from "../../audiobooks/applications/getAllAudiobooks";
-import { DeleteAudiobook } from "../../audiobooks/applications/deleteAudiobook";
-import { GetAudiobooksById } from "../../audiobooks/applications/getAudiobookById";
+import { CreateAudiobook } from "../../audiobooks/application/createAudiobook";
+import { GetAllAudiobooks } from "../../audiobooks/application/getAllAudiobooks";
+import { DeleteAudiobook } from "../../audiobooks/application/deleteAudiobook";
+import { GetAudiobooksById } from "../../audiobooks/application/getAudiobookById";
 import { GetBookByTheme } from "../../books/application/getBooksByTheme";
+import { MongoBookRepository } from "../../books/infrastructure/mongoBookRepository";
+import { GetAllBooksByLevel } from "../../books/application/getAllBooksByLevel";
 
 // * repositorio de la base de datos para uso de sus métodos de almacenamiento
 const booksRepository = new MongoBookRepository();
-const audiobooksRepository = new MongoAudiobooksRepository();
+const audiobooksRepository = new MongoAudiobookRepository();
 
 // ? contenedor que combina las aplicaciones de uso con los repositorios
 export const serviceContainer = {
-  
   // * métodos de solo libros
   book: {
-    createBooks: new BookCreate(booksRepository),
+    createBooks: new CreateBook(booksRepository),
     getAllBooks: new GetAllBooks(booksRepository),
     deleteBook: new DeleteBook(booksRepository),
     getBooksById: new GetBooksById(booksRepository),
@@ -30,11 +30,12 @@ export const serviceContainer = {
     getBooksBySubgenre: new GetBooksBySubgenre(booksRepository),
     getContentById: new GetContentBookById(booksRepository),
     getBookByTheme: new GetBookByTheme(booksRepository),
+    getAllBooksByLevel: new GetAllBooksByLevel(booksRepository),
   },
 
   // * métodos de solo audiolibros
   audiobooks: {
-    createAudioBooks: new CreateAudioBooks(audiobooksRepository),
+    createAudioBooks: new CreateAudiobook(audiobooksRepository),
     getAllAudioBooks: new GetAllAudiobooks(audiobooksRepository),
     deleteAudiobook: new DeleteAudiobook(audiobooksRepository),
     getAudiobookById: new GetAudiobooksById(audiobooksRepository),
