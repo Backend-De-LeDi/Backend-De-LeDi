3ea84d0f4f41e13f1d535aac6411395add64f006
ElBrunoX.js <brunolok79@gmail.com>
2025-07-07 20:15:13 -0300

    se creo la aplicación de uso para eliminar Audiolibros

---
+++ src/audiobooks/application/deleteAudiobook.ts
@@ -0,0 +1,14 @@
+import { AudiobooksRepository } from "../domain/AudiobooksRepository";
+import { Types } from "mongoose";
+
+// ? Clase que guía al Repositorio a realizar la eliminación del libro
+export class DeleteAudiobook {
+  // * Constructor que recibe el repositorio de libros
+  constructor(private repository: AudiobooksRepository) {}
+
+  // * Método que ejecuta la eliminación del libro por su ID
+  async run(id: Types.ObjectId): Promise<void> {
+    // * Llama al repositorio para eliminar el libro
+    await this.repository.deleteAudiobook(id);
+  }
+}
