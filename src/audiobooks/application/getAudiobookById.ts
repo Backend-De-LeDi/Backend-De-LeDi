c215280d89ccd3d8be378792d0d373ba8b983905
ElBrunoX.js <brunolok79@gmail.com>
2025-07-07 20:12:39 -0300

    se creo la aplicación de uso para obtener libros en base a su id

---
+++ src/audiobooks/application/getAudiobookById.ts
@@ -0,0 +1,14 @@
+import { Types } from "mongoose";
+import { SearchedAudiobook } from "../../types/audiobookTypes";
+import { AudiobooksRepository } from "../domain/AudiobooksRepository";
+// ? Clase que guía al Repositorio a realizar la obtención de un libro por su ID
+export class GetAudiobooksById {
+  // * Constructor que recibe el repositorio de libros
+  constructor(private repository: AudiobooksRepository) {}
+
+  // * Método que ejecuta la obtención del libro por su ID
+  async run(id: Types.ObjectId): Promise<SearchedAudiobook | null> {
+    // * Llama al repositorio para obtener el libro por su ID
+    return await this.repository.getAudiobookById(id);
+  }
+}
