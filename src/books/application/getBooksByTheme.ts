9f08b755a1b94ca9627acf86abdf0b9190842f15
ElBrunoX.js <brunolok79@gmail.com>
2025-07-07 20:40:35 -0300

    se cambio la el tipo e dato que va a devolver el getBookByTheme envés de Book es SearchedBook mas completo

--- src/books/application/getBooksByTheme.ts
+++ src/books/application/getBooksByTheme.ts
@@ -1,24 +1,21 @@
+import { SearchedBook } from "../../types/bookTypes";
 import { Books } from "../domain/books";
 import { BooksRepository } from "../domain/booksRepository";
 
 // ? clase que guía al Repositorio a realizar la obtención de libros por tema
 export class GetBookByTheme {
-
   // * constructor que recibe el repositorio de libros
   constructor(private repository: BooksRepository) {}
 
   // * método que se encarga de ejecutar la lógica de obtención de libros por tema
-  async run(theme: string[]): Promise<Books[]> {
-
+  async run(theme: string[]): Promise<SearchedBook[]> {
     // * se hace uso del repositorio para obtener los libros por tema
-  const books = await this.repository.getBookByTheme(theme);
+    const books = await this.repository.getBookByTheme(theme);
 
     // * si no hay libros, se retorna un array vacío
-  if (!books) return [];
-  
+    if (!books) return [];
 
     // * si hay libros, se retornan
-  return books;
-  
+    return books;
   }
 }
