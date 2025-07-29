80c5db4b9c84f0ae2f5e1c3c5d2d4f2642501691
ElBrunoX.js <brunolok79@gmail.com>
2025-07-29 20:17:35 -0300

    eliminación de comentarios

--- src/books/application/getBookById.ts
+++ src/books/application/getBookById.ts
@@ -1,15 +1,12 @@
 import { Types } from "mongoose";
-import { SearchedBook } from "../../types/bookTypes/bookTypes";
+import { SearchedBook } from "../../shared/types/bookTypes/bookTypes";
 import { BooksRepository } from "../domain/booksRepository";
 
 // ? Clase que guía al Repositorio a realizar la obtención de un libro por su ID
 export class GetBooksById {
-  // * Constructor que recibe el repositorio de libros
   constructor(private repository: BooksRepository) {}
 
-  // * Método que ejecuta la obtención del libro por su ID
   async run(id: Types.ObjectId): Promise<SearchedBook | null> {
-    // * Llama al repositorio para obtener el libro por su ID
     return await this.repository.getBookById(id);
   }
 }
