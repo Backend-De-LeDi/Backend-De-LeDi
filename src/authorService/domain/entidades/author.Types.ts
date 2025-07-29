0a48d7181d1752cd460bc1c7a81a22a0306979b6
ElBrunoX.js <brunolok79@gmail.com>
2025-07-29 20:14:04 -0300

    se formateo el código

--- src/authorService/domain/entidades/author.Types.ts
+++ src/authorService/domain/entidades/author.Types.ts
@@ -1,10 +1,6 @@
 import { photoProfile } from "../../../shared/types/photo.Types";
 
 export class Author {
-    public id: any;
-    constructor(
-        public readonly name: string,
-        public readonly biography: string,
-        public readonly avatar: photoProfile
-    ) { }
+  public id: any;
+  constructor(public readonly name: string, public readonly biography: string, public readonly avatar: photoProfile) {}
 }
