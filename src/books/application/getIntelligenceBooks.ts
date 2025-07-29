61fb66b8408a7d7f9c28bb1c58ee8c2789eac891
ElBrunoX.js <brunolok79@gmail.com>
2025-07-29 20:19:45 -0300

    eliminación de comentarios

--- src/books/application/getIntelligenceBooks.ts
+++ src/books/application/getIntelligenceBooks.ts
@@ -1,15 +1,13 @@
 import { BooksRepository } from "../domain/booksRepository";
 import { Books } from "../domain/books";
-import { SearchedBook } from "../../types/bookTypes/bookTypes";
+import { SearchedBook } from "../../shared/types/bookTypes/bookTypes";
 
 // ? Clase que guía al Repositorio a realizar la obtención de libros por inteligencia
 export class GetIntelligenceBook {
-  // * Constructor que recibe el repositorio de libros
   constructor(private repository: BooksRepository) {}
 
   // * Método que ejecuta la obtención de libros por inteligencia
-  async run(query: string): Promise<SearchedBook[]> {
-    // * Llama al repositorio para obtener los libros por inteligencia
-    return await this.repository.getIntelligenceBook(query);
+  async run(id: string[]): Promise<SearchedBook[]> {
+    return await this.repository.getIntelligenceBook(id);
   }
 }
