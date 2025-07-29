c844f6d6e726dc44511ad1986b9c08911ef91598
ElBrunoX.js <brunolok79@gmail.com>
2025-07-29 20:21:23 -0300

    se eliminaron dos especificaciones de la interfaz guía para los repositorios y caso de uso

--- src/books/domain/booksRepository.ts
+++ src/books/domain/booksRepository.ts
@@ -1,6 +1,6 @@
 import { Types } from "mongoose";
 import { Books } from "./books";
-import { SearchedBook } from "../../types/bookTypes/bookTypes";
+import { SearchedBook } from "../../shared/types/bookTypes/bookTypes";
 
 // ? Interfaz que define las operaciones del repositorio de libros
 export interface BooksRepository {
@@ -8,9 +8,8 @@ export interface BooksRepository {
   getAllBooks(): Promise<SearchedBook[]>;
   deleteBook(id: Types.ObjectId): Promise<void>;
   getBookById(id: Types.ObjectId): Promise<SearchedBook | null>;
-  getIntelligenceBook(query: string): Promise<SearchedBook[]>;
-  getBooksBySubgenre(subgenre: string[]): Promise<SearchedBook[]>;
+  getIntelligenceBook(id: string[]): Promise<SearchedBook[]>;
   getContentBookById(id: Types.ObjectId): Promise<string | null>;
-  getBookByTheme(theme: string[]): Promise<SearchedBook[]>;
   getAllBooksByLevel(nivel?: string): Promise<SearchedBook[]>;
+  getBooksByFiltering(theme: string[], subgenre: string[], yearBook: Date[], genre: string[]): Promise<SearchedBook[]>;
 }
