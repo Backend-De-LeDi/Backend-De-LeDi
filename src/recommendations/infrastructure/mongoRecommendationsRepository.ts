cf6108f2c3381dffe58af0d79e2ef28d57c5bddf
ElBrunoX.js <brunolok79@gmail.com>
2025-08-05 23:25:52 -0300

    renombrar el método recommendation a getBasicRecommendations y ajustar la implementación para obtener recomendaciones básicas

--- src/recommendations/infrastructure/mongoRecommendationsRepository.ts
+++ src/recommendations/infrastructure/mongoRecommendationsRepository.ts
@@ -2,11 +2,34 @@ import { SearchedBook } from "../../shared/types/bookTypes/bookTypes";
 import { RecommendationsRepository } from "../domain/recommendationsRepository";
 import { BookModel } from "../../books/infrastructure/model/books.model";
 import mongoose from "mongoose";
+import { format } from "morgan";
+import { BookProgressModel } from "../../userPogressBooks/infrastructure/models/BookProgressModel";
 
 export class MongoRecommendationsRepository implements RecommendationsRepository {
-  async recommendation(id: string[]): Promise<SearchedBook[]> {
-    const ids = id.map((x) => new mongoose.Types.ObjectId(x));
-    const recommendedBooks = await BookModel.find({ _id: { $in: ids } });
-    return recommendedBooks;
+  async getBasicRecommendations(themes: string[], format: string[]): Promise<SearchedBook[]> {
+    const recommendations = await BookModel.aggregate([
+      {
+        $addFields: {
+          themeMatches: { $setIntersection: ["$theme", themes] },
+          formatMatch: { $cond: [{ $in: ["$format", format] }, 1, 0] },
+        },
+      },
+      {
+        $addFields: {
+          matchScore: {
+            $add: [{ $size: "$themeMatches" }, "$formatMatch"],
+          },
+        },
+      },
+      { $match: { matchScore: { $gt: 0 } } },
+      { $sort: { matchScore: -1 } },
+    ]);
+    return recommendations;
+  }
+
+  async getAdvancedRecommendations(id: mongoose.Types.ObjectId): Promise<SearchedBook[]> {
+    const recommendations = await BookProgressModel.find({ idUser: id }).populate("idBook");
+    console.log(recommendations);
+    return recommendations as any;
   }
 }
