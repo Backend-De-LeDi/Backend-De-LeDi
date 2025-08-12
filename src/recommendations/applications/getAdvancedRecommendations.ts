d4e53a255535a4afcffb45560a1b74f05f6928df
ElBrunoX.js <brunolok79@gmail.com>
2025-08-12 14:00:52 -0300

    corrección de comentarios innecesarios

--- src/recommendations/applications/getAdvancedRecommendations.ts
+++ src/recommendations/applications/getAdvancedRecommendations.ts
@@ -2,7 +2,6 @@ import { Types } from "mongoose";
 import { SearchedBook } from "../../shared/types/bookTypes/bookTypes";
 import { RecommendationsRepository } from "../domain/recommendationsRepository";
 
-// ? caso de uso que maneja la lógica para obtener recomendaciones avanzadas de libros
 export class GetAdvancedRecommendations {
   constructor(private repository: RecommendationsRepository) {}
   async run(id: Types.ObjectId[], ids: Types.ObjectId[]): Promise<SearchedBook[]> {
