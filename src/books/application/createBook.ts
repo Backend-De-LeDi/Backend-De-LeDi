772a204c66808176c4cfdddaa6f2f3ff3f9c1015
ElBrunoX.js <brunolok79@gmail.com>
2025-08-12 13:58:50 -0300

    corregir: eliminar comentario innecesario en la clase CreateBook

--- src/books/application/createBook.ts
+++ src/books/application/createBook.ts
@@ -1,7 +1,6 @@
 import { Books } from "../domain/books";
 import { BooksRepository } from "../domain/booksRepository";
 
-// ? clase que guía al Repositorio a realizar el almacenamiento del libro
 export class CreateBook {
   constructor(private repository: BooksRepository) {}
 
