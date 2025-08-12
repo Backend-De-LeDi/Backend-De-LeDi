12df4d3a8b40b9fd3e25a78bacc92edd8c236499
ElBrunoX.js <brunolok79@gmail.com>
2025-08-12 13:59:24 -0300

    corregir: eliminar comentarios innecesarios en la clase GetContentBookById

--- src/books/application/getContentBookById.ts
+++ src/books/application/getContentBookById.ts
@@ -1,17 +1,10 @@
 import { Types } from "mongoose";
 import { BooksRepository } from "../domain/booksRepository";
 
-// ? Clase que guía al Repositorio a realizar la obtención del contenido de un libro por su ID
 export class GetContentBookById {
-
-  // * Constructor que recibe el repositorio de libros
   constructor(private repository: BooksRepository) {}
 
-  // * Método que ejecuta la obtención del contenido del libro por su ID
   async run(id: Types.ObjectId): Promise<string | null> {
-
-    // * Llama al repositorio para obtener el contenido del libro por su ID
     return await this.repository.getContentBookById(id);
-
   }
 }
