1f73963f35208033515a0987093fde80be0ae68f
ElBrunoX.js <brunolok79@gmail.com>
2025-08-12 13:59:01 -0300

    corregir: eliminar comentario innecesario en la clase GetAllBooks

--- src/books/application/getAllBooks.ts
+++ src/books/application/getAllBooks.ts
@@ -1,7 +1,6 @@
 import { SearchedBook } from "../../shared/types/bookTypes/bookTypes";
 import { BooksRepository } from "../domain/booksRepository";
 
-// ? Clase que guía al Repositorio a realizar la obtención de todos los libros
 export class GetAllBooks {
   constructor(private repository: BooksRepository) {}
 
