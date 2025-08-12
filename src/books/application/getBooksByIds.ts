f646c74c32ea15b317d186015638aa6f2200d003
ElBrunoX.js <brunolok79@gmail.com>
2025-08-12 13:59:18 -0300

    corregir: eliminar comentario innecesario en la clase GetBooksByIds

--- src/books/application/getBooksByIds.ts
+++ src/books/application/getBooksByIds.ts
@@ -2,7 +2,6 @@ import { SearchedBook } from "../../shared/types/bookTypes/bookTypes";
 import { BooksRepository } from "../domain/booksRepository";
 import { Types } from "mongoose";
 
-// ? caso de uso que permite obtener libros en base a una lista de ids proporcionados
 export class GetBooksByIds {
   constructor(private booksRepository: BooksRepository) {}
 
