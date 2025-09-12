ab7bc300173325fe1f5ba8e02f89f7cd95003504
ElBrunoX.js <brunolok79@gmail.com>
2025-09-12 20:24:59 -0300

    refactor: eliminar la interfaz token y optimizar la lógica de recomendaciones en RecommendationControllers

--- src/recommendations/interfaces/controllers/recommendationControllers.ts
+++ src/recommendations/interfaces/controllers/recommendationControllers.ts
@@ -1,14 +1,10 @@
 import { serviceContainer } from "../../../shared/services/serviceContainer";
 import { Request, Response } from "express";
-import { User } from "../../../userService/domain/entities/UserTypes";
 import mongoose from "mongoose";
-import { isValid } from "zod";
+import { FindProgressMongo } from "../../../userPogressBooks/infrastructure/bookProgressRepoMongo";
+import { token } from "../../../shared/types/IToken";
 
-interface token {
-  id: string;
-  iat: number;
-  exp: number;
-}
+const findProgresMongo = new FindProgressMongo
 
 export class RecommendationControllers {
   async getRecommendations(req: Request, res: Response): Promise<Response> {
@@ -16,8 +12,15 @@ export class RecommendationControllers {
 
     const idValid = new mongoose.Types.ObjectId(token.id);
 
-    // const recommendations = await serviceContainer.recommendations.getRecommendations.run(idValid);
+    const progrese = await findProgresMongo.findByUser(idValid)
 
+    if (progrese.length === 0) {
+      console.log("recomemdacion por preferencia");
+      const recommendations = await serviceContainer.recommendations.getRecommendations.run(idValid);
+      return res.status(200).json(recommendations);
+    }
+
+    console.log("recomendación semantica");
     const recommendations = await serviceContainer.recommendations.getRecommendatioSemantic.run(idValid)
     return res.status(200).json(recommendations);
   }
