c93f99227750babe56679d9aa23ff89f72c298c4
ElBrunoX.js <brunolok79@gmail.com>
2025-09-12 23:55:30 -0300

    feat: agregar clase GetRecommendatioSemantic para gestionar recomendaciones semánticas de libros

---
+++ src/recommendations/applications/getRecommendatioSemantic.ts
@@ -0,0 +1,11 @@
+import { Types } from "mongoose";
+import { RecommendationsRepository } from "../domains/recommendationsRepository";
+import { SearchedBook } from "../../shared/types/bookTypes/bookTypes";
+
+export class GetRecommendatioSemantic {
+     constructor(private repository: RecommendationsRepository) { }
+
+     async run(userId: Types.ObjectId): Promise<SearchedBook[]> {
+          return await this.repository.getRecommendatioSemantic(userId)
+     }
+}
