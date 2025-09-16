0ec765df5f4c137004d89cd09e55d2d2c76859ce
ElBrunoX.js <brunolok79@gmail.com>
2025-09-15 21:01:21 -0300

    se implemento una nueva funcionalidad en la interfaz principal del repositorio de la IA

--- src/ai/domain/AIRepository.ts
+++ src/ai/domain/AIRepository.ts
@@ -3,5 +3,5 @@ import { Types } from "mongoose"
 export interface AIRepository {
      getIdsForRecommendation(idsBooks: string[]): Promise<string[]>
      createYourHistoryGame(idBook: string): Promise<any>
-
+     createEmbedding(id: string, title: string, summary: string, synopsis: string): Promise<{ msg: string, status: boolean }>
 }
