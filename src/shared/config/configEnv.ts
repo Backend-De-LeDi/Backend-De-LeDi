73f42881f7cd3af792a9de44ab7a13afc911ab11
ElBrunoX.js <brunolok79@gmail.com>
2025-09-21 01:21:38 -0300

    nuevas variables de entorno

--- src/shared/config/configEnv.ts
+++ src/shared/config/configEnv.ts
@@ -7,6 +7,8 @@ interface Env {
   CLOUD_NAME: string | undefined;
   API_KEY: string | undefined;
   API_SECRET: string | undefined;
+  GEMINI_API_KEY: string | undefined
+  URL_GEMINI: string | undefined
 }
 
 const ENV: Env = {
@@ -15,6 +17,8 @@ const ENV: Env = {
   CLOUD_NAME: process.env.CLOUD_NAME,
   API_KEY: process.env.API_KEY,
   API_SECRET: process.env.API_SECRET,
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+  URL_GEMINI: process.env.URL_GEMINI
 };
 
 export default ENV;
