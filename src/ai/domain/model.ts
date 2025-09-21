e358104fc0cb0874e5a92d46c127c71171f08ae1
ElBrunoX.js <brunolok79@gmail.com>
2025-09-21 01:19:04 -0300

    intalacion y creacion de la varable que contendra el modelo

---
+++ src/ai/domain/model.ts
@@ -0,0 +1,8 @@
+import OpenAI from "openai"
+import ENV from "../../shared/config/configEnv"
+
+export const openai = new OpenAI({
+     apiKey: ENV.GEMINI_API_KEY,
+     baseURL: ENV.URL_GEMINI
+})
+
