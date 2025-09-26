fb6f8436defc86a6643f0ab4b70a2afbf7bc933b
ElBrunoX.js <brunolok79@gmail.com>
2025-09-26 15:54:42 -0300

    se agrego un nuevo parametro en el metodo createYourHistoryGame de la interfaz AIRepository

--- src/ai/domain/AIRepository.ts
+++ src/ai/domain/AIRepository.ts
@@ -1,7 +1,8 @@
 import { Types } from "mongoose";
+import { Gamble } from "../../shared/types/createYourHistory/createYourHistory";
 
 export interface AIRepository {
   getIdsForRecommendation(idsBooks: string[]): Promise<string[]>;
-  createYourHistoryGame(idBook: string): Promise<any>;
+  createYourHistoryGame(idBook: string, gamble: Gamble | undefined): Promise<any>;
   createEmbedding(id: Types.ObjectId, title: string, summary: string, synopsis: string): Promise<void>;
 }
