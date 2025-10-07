94d23ae80ee0441b6ba69641eb0cc489ca8539de
ElBrunoX.js <brunolok79@gmail.com>
2025-10-07 15:23:59 -0300

    cambios mínimos

--- src/authorService/app/service/SaveAuthor.service.ts
+++ src/authorService/app/service/SaveAuthor.service.ts
@@ -2,20 +2,15 @@ import { Author } from "../../domain/entidades/author.Types";
 import { FindAuthor } from "../../domain/ports/findAuthorRepository";
 import { ISaveAuthorRepository } from "../../domain/ports/saveAuthorRepository";
 
-
-
 export class CreateAuthor {
-    constructor(
-        private readonly saveAuthor: ISaveAuthorRepository,
-        private readonly uniqueAuthor: FindAuthor
-    ) { }
-    async saveAuthors(date: Author): Promise<Author> {
-        const authorExist = await this.uniqueAuthor.findByName(date.fullName)
-        if (authorExist) {
-            throw new Error("author already in use");
-        }
-
-        const saveAuthors = await this.saveAuthor.crateAuthor(date)
-        return saveAuthors
+  constructor(private readonly saveAuthor: ISaveAuthorRepository, private readonly uniqueAuthor: FindAuthor) {}
+  async saveAuthors(date: Author): Promise<Author> {
+    const authorExist = await this.uniqueAuthor.findByName(date.fullName);
+    if (authorExist) {
+      throw new Error("author already in use");
     }
+
+    const saveAuthors = await this.saveAuthor.crateAuthor(date);
+    return saveAuthors;
+  }
 }
