704e600e73023de7ca0d1408f1df7f5821c4617b
ElBrunoX.js <brunolok79@gmail.com>
2025-10-10 21:08:38 -0300

    se atuclizo la logica para que se puedan crear autores en supabase

--- src/authorService/app/service/SaveAuthor.service.ts
+++ src/authorService/app/service/SaveAuthor.service.ts
@@ -3,14 +3,22 @@ import { FindAuthor } from "../../domain/ports/findAuthorRepository";
 import { ISaveAuthorRepository } from "../../domain/ports/saveAuthorRepository";
 
 export class CreateAuthor {
-  constructor(private readonly saveAuthor: ISaveAuthorRepository, private readonly uniqueAuthor: FindAuthor) {}
-  async saveAuthors(date: Author): Promise<Author> {
+  constructor(private readonly saveAuthor: ISaveAuthorRepository[], private readonly uniqueAuthor: FindAuthor) { }
+  async saveAuthors(date: Author): Promise<(Author | (Author & number[])[])[]> {
     const authorExist = await this.uniqueAuthor.findByName(date.fullName);
     if (authorExist) {
-      throw new Error("author already in use");
+      return [];
     }
 
-    const saveAuthors = await this.saveAuthor.crateAuthor(date);
-    return saveAuthors;
+    const saveAuthorsMogo = await this.saveAuthor[0].createAuthor(date);
+    const saveAuthorsSupabase = await this.saveAuthor[1].createAuthor(saveAuthorsMogo as Author);
+    return [saveAuthorsMogo, saveAuthorsSupabase] as (Author & (Author & number[])[])
+  }
+}
+
+export class SaveAuthorSupabase {
+  constructor(private readonly saveAuthor: ISaveAuthorRepository) { }
+  async saveAuthors(date: Author): Promise<(Author | (Author & number[])[])> {
+    return await this.saveAuthor.createAuthor(date)
   }
 }
