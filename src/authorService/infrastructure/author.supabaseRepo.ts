87923ca1969008081d5770358021538b3be345dd
ElBrunoX.js <brunolok79@gmail.com>
2025-10-10 21:14:08 -0300

    creacion de authores en supabase

--- src/authorService/infrastructure/author.supabaseRepo.ts
+++ src/authorService/infrastructure/author.supabaseRepo.ts
@@ -2,6 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 import ENV from "../../shared/config/configEnv";
 import { ISaveAuthorRepository } from "../domain/ports/saveAuthorRepository";
 import { Author } from "../domain/entidades/author.Types";
+import saveWritingGenre from "../../writingGenre/helpers/saveWritingGenre";
+import { number } from "zod";
 
 const supabaseUrl = ENV.URL_SUPABASE;
 const supabaseKey = ENV.SUPABASE_KEY;
@@ -11,14 +13,35 @@ if (!supabaseUrl || !supabaseKey) throw new Error("Supabase URL or Key is not de
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export class AuthorSupabaseRepo implements ISaveAuthorRepository {
-  async crateAuthor(author: Author): Promise<Author> {
-    const { data, error } = await supabase.from("author").insert([author]).select("*");
+
+  async createAuthor(author: Author): Promise<(Author & number[])[]> {
+    const newAuthor = {
+      idMongo: author._id,
+      biography: author.biography,
+      profession: author.profession,
+      birthdate: author.birthdate,
+      birthplace: author.birthplace,
+      nationality: author.nationality,
+      fullName: author.fullName,
+    }
+
+    let genres: number[] = [];
+
+    if (author.writingGenre && author.writingGenre.length > 0) {
+      genres = await saveWritingGenre.run(author.writingGenre);
+    }
+
+    const { data, error } = await supabase.from("author").insert([newAuthor]).select("*");
 
     if (error) {
       console.error("Error al insertar:", error);
       throw new Error("Supabase insert error: " + error.message);
     }
+    const authorData = { ...data?.[0], _id: data[0].id };
+    return [authorData, genres] as (Author & number[])[];
+  }
 
-    return data?.[0];
+  async deleteAuthor(id: any): Promise<void> {
+    await supabase.from("author").delete().eq("idMongo", id);
   }
 }
