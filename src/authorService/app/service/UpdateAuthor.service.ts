fc2ed94b295652875ff134b3fa24b2efb88982ad
ElBrunoX.js <brunolok79@gmail.com>
2025-10-10 21:09:20 -0300

    test de prueba de actualizar los autores en supabase

--- src/authorService/app/service/UpdateAuthor.service.ts
+++ src/authorService/app/service/UpdateAuthor.service.ts
@@ -3,17 +3,19 @@ import { FindAuthor } from "../../domain/ports/findAuthorRepository";
 import { UpdateAuthorRepository } from "../../domain/ports/updateAuthorRepository";
 
 
-
 export class UpdateAuthor implements UpdateAuthorRepository {
     constructor(private readonly updateAuthors: UpdateAuthorRepository,
-        private readonly uniqueAuthor: FindAuthor
+        private readonly uniqueAuthor: FindAuthor,
     ) { }
 
     async updateAuthor(id: any, author: Author): Promise<Author | null> {
+        console.log(author);
+
         const authorExist = await this.uniqueAuthor.findByName(author.fullName);
         if (authorExist) {
-            throw new Error("Email already in use");
+            return null
         }
+
         return await this.updateAuthors.updateAuthor(id, author);
     }
 }
