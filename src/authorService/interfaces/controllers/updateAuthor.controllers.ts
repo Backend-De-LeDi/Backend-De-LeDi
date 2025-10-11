7e98bd9932edd6f0f5a47e7c125166205512f682
ElBrunoX.js <brunolok79@gmail.com>
2025-10-10 21:16:54 -0300

    mejora en la lógica de actualización de autores y manejo de errores en Supabase

--- src/authorService/interfaces/controllers/updateAuthor.controllers.ts
+++ src/authorService/interfaces/controllers/updateAuthor.controllers.ts
@@ -2,22 +2,37 @@ import { Request, Response } from "express";
 import { UpdateAuthor } from "../../app/service/UpdateAuthor.service";
 import { findAuthorMongoRepo, updateAuthorMongo } from "../../infrastructure/authores.MongoRepo";
 import { Author } from "../../domain/entidades/author.Types";
-
+import { DeleteAuthorsSupabase } from "../../app/service/DeleteAuthor.service";
+import { AuthorSupabaseRepo } from "../../infrastructure/author.supabaseRepo";
+import { SaveAuthorSupabase } from "../../app/service/SaveAuthor.service";
 
 const updateAuthorRepo = new updateAuthorMongo();
 const findAuthorRepo = new findAuthorMongoRepo()
+const deleteAuthorRepo = new DeleteAuthorsSupabase(new AuthorSupabaseRepo());
+const saveAuthorSupabase = new SaveAuthorSupabase(new AuthorSupabaseRepo());
 const updataAuthor = new UpdateAuthor(updateAuthorRepo, findAuthorRepo);
 
 export const updataAuthors = async (req: Request, res: Response) => {
     try {
+        console.log(req.body);
+
         const newAuthor: Author = req.body
+        console.log(newAuthor);
+
         const { id } = req.params
 
-        const result = await updataAuthor.updateAuthor(id, newAuthor)
-        if (!result) {
+        const resultMongo = await updataAuthor.updateAuthor(id, newAuthor);
+        await deleteAuthorRepo.deleteAuthor(id)
+        const resultSupabase = saveAuthorSupabase.saveAuthors(newAuthor)
+        if (!resultMongo) {
             res.status(302).json({ msg: "the author not update" });
+            return
         }
+        if (!resultSupabase) console.warn(`error al actualizar el author con id: ${id} en supabases`);
+
+
         res.status(200).json({ msg: "author update successful" });
+        return
     } catch (error) {
         console.log(error);
 
