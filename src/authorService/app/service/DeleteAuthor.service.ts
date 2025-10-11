f9e80609837423b51866b9cf5773348367f75ea1
ElBrunoX.js <brunolok79@gmail.com>
2025-10-10 21:07:35 -0300

    se creo la clase de caso de uso que eliminara de supabase los autores

--- src/authorService/app/service/DeleteAuthor.service.ts
+++ src/authorService/app/service/DeleteAuthor.service.ts
@@ -6,3 +6,10 @@ export class DeleteAuthors implements DeleteAuthor {
         await this.deleteAuthors.deleteAuthor(id)
     }
 }
+
+export class DeleteAuthorsSupabase implements DeleteAuthor {
+    constructor(private readonly deleteAuthors: DeleteAuthor) { }
+    async deleteAuthor(id: any): Promise<void> {
+        await this.deleteAuthors.deleteAuthor(id)
+    }
+};
