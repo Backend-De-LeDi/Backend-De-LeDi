3b6769767f5534ade3a8257a8da260debbb1e37c
ElBrunoX.js <brunolok79@gmail.com>
2025-10-10 21:16:58 -0300

    mejora en la duración del token JWT a 1 día

--- src/authService/helpers/generJWT.ts
+++ src/authService/helpers/generJWT.ts
@@ -6,7 +6,7 @@ export const generarJWT = (id: any, rol: string) => {
   return new Promise((resolve, reject) => {
     const payload = { id, rol };
     console.log(payload);
-    jwt.sign(payload, JWT_SECRET as string, { expiresIn: "1h" }, (err, token) => {
+    jwt.sign(payload, JWT_SECRET as string, { expiresIn: "1d" }, (err, token) => {
       if (err) {
         console.error("Error generating JWT:", err);
         reject(err);
