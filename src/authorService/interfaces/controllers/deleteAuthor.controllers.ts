d7ead705aea5b71066a3cb17a790499ebe804a7e
ElBrunoX.js <brunolok79@gmail.com>
2025-10-10 21:15:43 -0300

    se mejora la logica de que si no hay autor retonara una respuesta de que no a autor para eliminar

--- src/authorService/interfaces/controllers/deleteAuthor.controllers.ts
+++ src/authorService/interfaces/controllers/deleteAuthor.controllers.ts
@@ -1,17 +1,25 @@
 import { Request, Response } from "express";
 import { DeleteAuthors } from "../../app/service/DeleteAuthor.service";
 import { DeleteAuthorMongoRepo } from "../../infrastructure/authores.MongoRepo";
+import { AuthorSupabaseRepo } from "../../infrastructure/author.supabaseRepo";
 
 
 // new instances of classes 
 const deleteAuthorRepo = new DeleteAuthorMongoRepo();
+const authorSupabaseRepo = new AuthorSupabaseRepo();
 const deleteAuthorService = new DeleteAuthors(deleteAuthorRepo);
+const deleteAuthorSupabaseService = new DeleteAuthors(authorSupabaseRepo);
 
 //delte author
 export const deleteAuthorById = async (req: Request, res: Response) => {
     try {
         const { id } = req.params;
-        await deleteAuthorService.deleteAuthor(id);
+        const result = await deleteAuthorService.deleteAuthor(id);
+        if (result === null) {
+            res.status(404).json({ message: "Author not found" });
+            return
+        }
+        await deleteAuthorSupabaseService.deleteAuthor(id);
         res.status(200).json({ message: "Author deleted successfully" });
 
     } catch (error) {
