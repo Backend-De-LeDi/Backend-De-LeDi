b7c24d992c6022977f847c53d9b240dbb9d12c64
ElBrunoX.js <brunolok79@gmail.com>
2025-10-20 23:29:36 -0300

    creamos los helper que nos permite utilizar los caso de uso con el repositorio de la IA

--- src/ai/helper/saveForVectorStore.ts
+++ src/ai/helper/saveForVectorStore.ts
@@ -1,6 +1,8 @@
-import { SaveForVectorStore } from "../applications/embeddings/saveForVectorStore";
+import { SaveForVectorStore, SaveForVectorStoreAuthors } from "../applications/embeddings/saveForVectorStore";
 import { ConnectionAI } from "../infrastructure/serviceOfAI";
-import { DeleteBookFromDocuments } from "../applications/embeddings/deleteBookFromDocuments";
+import { DeleteAuthorFromDocuments, DeleteBookFromDocuments } from "../applications/embeddings/deleteBookFromDocuments";
 
 export const serviceAi = new SaveForVectorStore(new ConnectionAI())
+export const serviceAiAuthor = new SaveForVectorStoreAuthors(new ConnectionAI())
 export const deleteBookFromDocuments = new DeleteBookFromDocuments(new ConnectionAI())
+export const deleteAuthorFromDocuments = new DeleteAuthorFromDocuments(new ConnectionAI())
