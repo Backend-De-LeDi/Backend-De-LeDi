4c2cba526c7e6ef09ab64ddde88d40f72e42b815
ElBrunoX.js <brunolok79@gmail.com>
2025-11-07 21:25:29 -0300

    implementacion de mejora en la logica de avatar y implementacion de la logica de actualizacion

--- src/avatars/infrastructure/avatar.mongoRepository.ts
+++ src/avatars/infrastructure/avatar.mongoRepository.ts
@@ -2,25 +2,36 @@ import { Types } from "mongoose";
 import { AvatarType } from "../domain/entities/AvatarsTypes";
 import { IAvatar } from "../domain/ports/AvatarPorts";
 import { AvatarModel } from "./models/avatarModel";
+import { deleteCoverImage } from "../../shared/utils/deleteCoverImage";
 
 
 
 
 export class AvatarMongoRepository implements IAvatar {
-    async saveAvatar(avatar: AvatarType): Promise<AvatarType> {
-        const newAvatar = new AvatarModel(avatar);
-        return await newAvatar.save()
-    }
-    async findAvatars(): Promise<AvatarType[]> {
-        const result = await AvatarModel.find()
-        return result
-    }
+	async saveAvatar(avatar: AvatarType): Promise<AvatarType> {
+		const newAvatar = new AvatarModel(avatar);
+		return await newAvatar.save()
+	}
+	async findAvatars(): Promise<AvatarType[]> {
+		const result = await AvatarModel.find()
+		return result
+	}
 
-    async deleteAvatar(id: Types.ObjectId): Promise<void> {
-        const result = await AvatarModel.findByIdAndDelete(id)
+	async deleteAvatar(id: Types.ObjectId): Promise<void | null> {
+		const result = await AvatarModel.findById(id);
+		if (!result) {
+			return null
+		}
+		await deleteCoverImage(result.avatars.id_image);
 
-    }
-    async findAvatarById(id: any): Promise<AvatarType | null> {
-        return await AvatarModel.findById(id)
-    }
+		await AvatarModel.findByIdAndDelete(result._id);
+
+	}
+	async findAvatarById(id: any): Promise<AvatarType | null> {
+		return await AvatarModel.findById(id)
+	}
+
+	async updateAvatar(id: Types.ObjectId, avatar: AvatarType): Promise<AvatarType | null> {
+		return await AvatarModel.findByIdAndUpdate(id, avatar, { new: true });
+	}
 }
